f0401d4cd770ce3618b2472936fec6df3cd9b6d7
Marek <marecki.mail@gmail.com>
2021-11-08 12:44:38 +0100

    Update api-const.js

--- src/const/api-const.js
+++ src/const/api-const.js
@@ -1,5 +1,5 @@
 export const API_NAME = 'xmass app';
-export const API_URL = 'http://localhost:5000';
-// export const API_URL = 'http://xmass-app.herokuapp.com';
+// export const API_URL = 'http://localhost:5000';
+export const API_URL = 'http://xmass-app.herokuapp.com';
 
 export default API_URL;
